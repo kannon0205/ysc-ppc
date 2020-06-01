7df7aea9941b5ae87f54266baa9d158ae2d782ec
kamon <kannon0205@gmail.com>
2020-06-01 10:14:00 +0900

    stringを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_004717) do
+ActiveRecord::Schema.define(version: 2020_06_01_011144) do
 
   create_table "games", force: :cascade do |t|
     t.string "player_a"
